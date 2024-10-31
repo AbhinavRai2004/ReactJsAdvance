0054014c1e173d71d627203e8045ce283c70fc23
Abhinav Rai <111674442+AbhinavRai2004@users.noreply.github.com>
2024-10-31 18:55:47 +0530

    Delete Food_App directory

--- Food_App/src/components/Grocery.js
+++
@@ -1,9 +0,0 @@
-const Grocery = () => {
-  return (
-    <div>
-      <h1>This is a Grocery mart</h1>
-    </div>
-  );
-};
-
-export default Grocery;
--- Food_App/src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const CDN_URL =
-  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-
-export const LOGO_URL =
-  "https://i.fbcd.co/products/original/d32491363c1d52ac365372cd2df281d6a7cf44f8873fa0900cd4a78a1528628c.jpg";
-
-export const MENU_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
